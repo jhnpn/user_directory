d21f72e3e888c3af0d3c7fd7acc3f6299e127e7e
umaiibo <j.hnpn123@gmail.com>
2020-06-30 20:20:03 -0700

    added necessary files needed for set up

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardBody from '../CardBody';
+import CardHeading from '../CardHeading';
+import './style.css';
+
+function Card() {
+    return (
+        <div>
+           <CardHeading />
+           <CardBody /> 
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/components/CardHeading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function CardHeading() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardHeading;
